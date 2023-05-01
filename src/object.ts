6ef11486e8f822bce140f2641795f28ec49c9699
monaulic <sohelmuhammad164@gmail.com>
2023-05-01 09:55:17 +0600

    objects explain and readonly and some specific name defined

---
+++ src/object.ts
@@ -0,0 +1,15 @@
+const user :  {
+    company: "Programming Hero",
+    name : string,
+    readonly age : number,
+    isMarried : boolean,
+    wife? : string
+} = {
+    name : "Monirul",
+    age : 23,
+    isMarried : false,
+    company: "Programming Hero",
+    wife : undefined
+}
+
+// user.age = 34
